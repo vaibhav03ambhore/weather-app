8e593ab2bdaacb553c8f6a9c1da2f5756cd5bbaa
Vaibhav Ambhore <115728010+vaibhav03ambhore@users.noreply.github.com>
2024-05-18 10:19:45 +0530

    Add files via upload
    apps files uploaded

---
+++ app.js
@@ -0,0 +1,105 @@
+
+const iconElement = document.querySelector(".weather-icon")
+const locationIcon = document.querySelector(".location-icon")
+const tempElement = document.querySelector(".temprature-value p")
+const descElement = document.querySelector(".temprature-description p")
+const locationElement = document.querySelector(".location p")
+const notificationElement = document.querySelector(".notification")
+
+var input= document.getElementById("search")
+let city =""
+let latitude= 0.0
+let longitude= 0.0
+
+
+input.addEventListener("keyup" , function(event) {
+
+    if(event.keyCode ===13 ){
+        event.preventDefault();
+
+        city=input.value
+        getSearchWeather(city)
+        console.log(city)
+    }
+})
+const weather ={}
+
+weather.temprature ={
+    unit: "celsius"
+}
+
+const KELVIN =273
+
+const key='da9ea3a015e090bdec28882fd80b2578'
+
+if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition,showError)
+} else {
+    notificationElement.style.display ='block'
+    notificationElement.innerHTML ='<p> Browser doesnt support geolocation </p>'
+}
+
+function setPosition(position) {
+    latitude =position.coords.latitude
+    longitude =position.coords.longitude
+
+    getWeather(latitude,longitude)
+}
+locationIcon.addEventListener("click" ,function(event){
+    console.log('clicked on loc-icon',latitude,longitude)
+    getWeather(latitude,longitude)
+})
+
+function showError(error) {
+    notificationElement.style.display="block"
+    notificationElement.innerHTML= `<p> ${error.message} </p>`
+}
+function getSearchWeather(city) {
+    let api= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+
+    fetch(api)
+    .then(function (response) {
+        let data= response.json()
+        console.log(data)
+        return data
+    })
+    .then(function( data) {
+        weather.temprature.value=Math.floor(data.main.temp -KELVIN)
+        weather.description=data.weather[0].description
+        weather.iconId= data.weather[0].icon
+        weather.city= data.name
+        weather.country= data.sys.country
+    })
+    .then(function() {
+        displayWeather()
+    })
+}
+function getWeather(latitude,logitude) {
+   let api= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${logitude}&appid=${key}`
+
+   fetch(api)
+    .then(function (response) {
+        let data= response.json()
+        console.log(data)
+        return data
+    })
+    .then(function( data) {
+        weather.temprature.value=Math.floor(data.main.temp -KELVIN)
+        weather.description=data.weather[0].description
+        weather.iconId= data.weather[0].icon
+        weather.city= data.name
+        weather.country= data.sys.country
+    })
+    .then(function() {
+        displayWeather()
+    })
+}
+
+function displayWeather() {
+
+    iconElement.innerHTML=`<img src="icons/${weather.iconId}.png"/>`
+    tempElement.innerHTML=`${weather.temprature.value} *<span>C<span>`
+    descElement.innerHTML=weather.description
+    locationElement.innerHTML=`${weather.city}, ${weather.country}`
+}
+
