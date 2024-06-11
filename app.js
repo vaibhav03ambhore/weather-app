50881f4fa710b7624aa50dd563af688a2ee69475
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-06-11 14:26:54 +0530

    update and precise

--- app.js
+++ app.js
@@ -1,105 +1,91 @@
+const iconElement = document.querySelector(".weather-icon img");
+const locationIcon = document.querySelector(".location-icon img");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+const input = document.getElementById("search");
 
-const iconElement = document.querySelector(".weather-icon")
-const locationIcon = document.querySelector(".location-icon")
-const tempElement = document.querySelector(".temprature-value p")
-const descElement = document.querySelector(".temprature-description p")
-const locationElement = document.querySelector(".location p")
-const notificationElement = document.querySelector(".notification")
+let city = "";
+let latitude = 0.0;
+let longitude = 0.0;
 
-var input= document.getElementById("search")
-let city =""
-let latitude= 0.0
-let longitude= 0.0
-
-
-input.addEventListener("keyup" , function(event) {
-
-    if(event.keyCode ===13 ){
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
         event.preventDefault();
-
-        city=input.value
-        getSearchWeather(city)
-        console.log(city)
+        city = input.value;
+        getSearchWeather(city);
+        console.log(city);
     }
-})
-const weather ={}
+});
 
-weather.temprature ={
-    unit: "celsius"
-}
+const weather = {
+    temperature: {
+        unit: "celsius"
+    }
+};
 
-const KELVIN =273
+const KELVIN = 273;
+const key = 'da9ea3a015e090bdec28882fd80b2578';
 
-const key='da9ea3a015e090bdec28882fd80b2578'
-
-if("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(setPosition,showError)
+if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
 } else {
-    notificationElement.style.display ='block'
-    notificationElement.innerHTML ='<p> Browser doesnt support geolocation </p>'
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = '<p>Browser doesn\'t support geolocation</p>';
 }
 
 function setPosition(position) {
-    latitude =position.coords.latitude
-    longitude =position.coords.longitude
-
-    getWeather(latitude,longitude)
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    getWeather(latitude, longitude);
 }
-locationIcon.addEventListener("click" ,function(event){
-    console.log('clicked on loc-icon',latitude,longitude)
-    getWeather(latitude,longitude)
-})
+
+locationIcon.addEventListener("click", function(event) {
+    console.log('Clicked on loc-icon', latitude, longitude);
+    getWeather(latitude, longitude);
+});
 
 function showError(error) {
-    notificationElement.style.display="block"
-    notificationElement.innerHTML= `<p> ${error.message} </p>`
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p>${error.message}</p>`;
 }
+
 function getSearchWeather(city) {
-    let api= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    
     fetch(api)
-    .then(function (response) {
-        let data= response.json()
-        console.log(data)
-        return data
-    })
-    .then(function( data) {
-        weather.temprature.value=Math.floor(data.main.temp -KELVIN)
-        weather.description=data.weather[0].description
-        weather.iconId= data.weather[0].icon
-        weather.city= data.name
-        weather.country= data.sys.country
-    })
-    .then(function() {
-        displayWeather()
-    })
+        .then(response => response.json())
+        .then(data => {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            displayWeather();
+        })
+        .catch(error => showError(error));
 }
-function getWeather(latitude,logitude) {
-   let api= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${logitude}&appid=${key}`
 
-   fetch(api)
-    .then(function (response) {
-        let data= response.json()
-        console.log(data)
-        return data
-    })
-    .then(function( data) {
-        weather.temprature.value=Math.floor(data.main.temp -KELVIN)
-        weather.description=data.weather[0].description
-        weather.iconId= data.weather[0].icon
-        weather.city= data.name
-        weather.country= data.sys.country
-    })
-    .then(function() {
-        displayWeather()
-    })
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(response => response.json())
+        .then(data => {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            displayWeather();
+        })
+        .catch(error => showError(error));
 }
 
 function displayWeather() {
-
-    iconElement.innerHTML=`<img src="icons/${weather.iconId}.png"/>`
-    tempElement.innerHTML=`${weather.temprature.value} *<span>C<span>`
-    descElement.innerHTML=weather.description
-    locationElement.innerHTML=`${weather.city}, ${weather.country}`
+    iconElement.src = `icons/${weather.iconId}.png`;
+    tempElement.innerHTML = `${weather.temperature.value} <span>°C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
-
